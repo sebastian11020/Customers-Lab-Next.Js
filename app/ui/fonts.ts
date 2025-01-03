267ee62d0b8a16ec93bab6fd1f1e83ff97c8eba1
Sebastian <Sebastiandd87@gmail.com>
2025-01-03 10:19:07 -0500

    Initial Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ 
+    subsets: ['latin'], 
+    display: 'swap'
+});
+
+export const lusitana = Lusitana ({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: '400'
+});
